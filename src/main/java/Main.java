a1cd23e61d06feb528c8d9b3ba5ecf0bb0422367
Adrian Khaskin <haskin.dev@gmail.com>
2022-06-28 13:29:33 -0400

    small edit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,6 @@ public class Main {
     }
 
     public static void getUserBooks() {
-        String finishedPrompt = "Are you done adding books(Y/n): ";
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 String title = "", genre = "";
